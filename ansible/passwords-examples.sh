c50da39d020c959da11908707526e8e163e24fd1
Anton Valishin <anton@valishin.ru>
2025-08-17 01:52:54 +0300

    small fixes

--- ansible/passwords-examples.sh
+++ ansible/passwords-examples.sh
@@ -1,1 +1,2 @@
+secret-tool store --label='ansible-vault-dev' xdg:schema org.freedesktop.Secret.Generic account  ansible-vault-dev
 ansible-vault encrypt_string  --encrypt-vault-id dev  'ftppass123!ChangeMe' --name 'ftp_db_pass'
--- ansible/vault-pass.py
+++ ansible/vault-pass.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 # ansible-vault edit --vault-password-file=./vault-pass-secret-service.py 
-# secret-tool store --label='ansible-vault' xdg:schema org.freedesktop.Secret.Generic account ansible-vault
-#!/usr/bin/env python3
+# secret-tool store --label='ansible-vault-dev' xdg:schema org.freedesktop.Secret.Generic account  ansible-vault-dev
 import os, sys, gi
 gi.require_version('Secret', '1')
 from gi.repository import Secret
