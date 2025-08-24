4ed3bc2dc5d18801e03b90c7192259f505bf3442
Anton Valishin <anton@valishin.ru>
2025-08-24 21:55:05 +0300

    refactor for redability

--- ansible/vault-pass.py
+++ ansible/vault-pass.py
@@ -1,56 +1,78 @@
 #!/usr/bin/env python3
-# secret-tool store --label='ansible-vault-dev' xdg:schema org.freedesktop.Secret.Generic account  ansible-vault-dev
-# ansible-vault create  --vault-id dev@./vault-pass-dev.py dev-secret.yml
+"""
+Retrieve an Ansible Vault password from the system keyring using libsecret.
+
+Usage:
+    secret-tool store --label='ansible-vault-dev' \
+        xdg:schema org.freedesktop.Secret.Generic ansible-vault-attr ansible-vault-dev
+"""
+
 import os
 import sys
 import ctypes as ct
 
 libsecret = ct.CDLL("libsecret-1.so.0")
 
+
+def main():
+    vault_id = get_vault_id()
+    account_value = f"ansible-vault-{vault_id}".encode()
+    schema = build_schema()
+    password = lookup_password(schema, account_value)
+    print(password)
+
 SECRET_SCHEMA_NONE = 0
 SECRET_SCHEMA_ATTRIBUTE_STRING = 0
+ATTRIBUTE=b"ansible-vault-attr"
 
 class SecretSchemaAttribute(ct.Structure):
     _fields_ = [
         ("name", ct.c_char_p),
-        ("type", ct.c_uint)
+        ("type", ct.c_uint),
     ]
 
 class SecretSchema(ct.Structure):
     _fields_ = [
         ("name", ct.c_char_p),
         ("flags", ct.c_uint),
-        ("attributes", SecretSchemaAttribute * 32)
+        ("attributes", SecretSchemaAttribute * 32),
     ]
 
 libsecret.secret_password_lookup_sync.argtypes = [
-    ct.POINTER(SecretSchema),
-    ct.c_void_p,
-    ct.c_void_p
+    ct.POINTER(SecretSchema),  # schema
+    ct.c_void_p,               # cancellable
+    ct.c_void_p,               # error
 ]
 libsecret.secret_password_lookup_sync.restype = ct.c_char_p
 
+def build_schema() -> SecretSchema:
+    schema = SecretSchema()
+    schema.name = b"org.freedesktop.Secret.Generic"
+    schema.flags = SECRET_SCHEMA_NONE
+    schema.attributes[0] = SecretSchemaAttribute(ATTRIBUTE, SECRET_SCHEMA_ATTRIBUTE_STRING)
+    schema.attributes[1] = SecretSchemaAttribute(None, 0)  # terminator
+    return schema
 
-schema = SecretSchema()
-schema.name = b"org.freedesktop.Secret.Generic"
-schema.flags = SECRET_SCHEMA_NONE
-schema.attributes[0] = SecretSchemaAttribute(b"account", SECRET_SCHEMA_ATTRIBUTE_STRING)
-schema.attributes[1] = SecretSchemaAttribute(None, 0)
+def get_vault_id() -> str:
+    return os.path.basename(sys.argv[0]).removeprefix("vault-pass-").removesuffix(".py")
 
-vault_id = os.path.basename(sys.argv[0]).removeprefix("vault-pass-").removesuffix(".py")
-account_value = f"ansible-vault-{vault_id}".encode()
-err = ct.c_int()
+def lookup_password(schema: SecretSchema, account: bytes) -> str:
+    err = ct.c_int()
+    pw_ptr = libsecret.secret_password_lookup_sync(
+        ct.byref(schema),
+        None,
+        ct.byref(err),
+        ATTRIBUTE, account,
+        None
+    )
 
-pw_ptr = libsecret.secret_password_lookup_sync(
-    ct.byref(schema),
-    None,
-    ct.byref(err),
-    b"account", account_value,
-    None
-)
+    if not pw_ptr or err.value != 0:
+        sys.stderr.write(f"No secret found for account '{account.decode()}'\n")
+        sys.exit(1)
 
-if not pw_ptr and err.value != 0:
-    sys.stderr.write(f"No secret for ansible vault '{vault_id}'\n")
-    sys.exit(1)
+    return ct.string_at(pw_ptr).decode()
 
-print(ct.string_at(pw_ptr).decode())
+
+
+if __name__ == "__main__":
+    main()
